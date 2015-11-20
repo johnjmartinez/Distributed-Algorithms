9cb8af144da6870118c1b81eb8fef7cdc933096b
andresjaramillo23 <a.jaramillo24@gmail.com>
2015-11-20 16:07:02 -0600

    para john

---
+++ MyApplication2/app/src/main/java/comedor/myapplication/MainActivity.java
@@ -0,0 +1,72 @@
+package comedor.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void display (View view) {
+        EditText clave = (EditText) findViewById(R.id.editText6);
+        EditText name = (EditText) findViewById(R.id.editText2);
+        EditText password = (EditText) findViewById(R.id.editText4);
+        RadioButton boton = (RadioButton) findViewById(R.id.radioButton);
+
+        String user = name.getText().toString();
+        String pwd = password.getText().toString();
+
+
+        if (pwd.equals("rosales") && user.equals("999")) {
+            boton.setVisibility(View.VISIBLE);
+        }
+        else {
+            clave.setVisibility(View.VISIBLE);
+        }
+    }
+}
